d154c893cfe56124ea68d9b73b41a9c08435e96e
HaitongC <haitongchen116@gmail.com>
2024-04-01 13:36:03 -0400

    setup

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# install dependencies
+conda install --yes -c pytorch pytorch=1.7.1 torchvision cudatoolkit=11.0
+pip install ftfy regex tqdm
+pip install git+https://github.com/openai/CLIP.git
